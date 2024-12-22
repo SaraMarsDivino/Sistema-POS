6fcefe4df631f68afe48cbab1bb9b56f59e53bfb
ROCHA007 gameplaysdecalidad <rochaulin2@gmail.com>
2024-12-22 17:32:35 -0300

    main version casi final

--- cashier/urls.py
+++ cashier/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.cashier_dashboard, name='cashier_dashboard'),
     path('abrir-caja/', views.abrir_caja, name='abrir_caja'),
     path('cerrar_caja/', views.cerrar_caja, name='cerrar_caja'),
+
     path('buscar-producto/', views.buscar_producto, name='buscar_producto'),
     path('ajustar-cantidad/', views.ajustar_cantidad, name='ajustar_cantidad'),
     path('agregar-al-carrito/', views.agregar_al_carrito, name='agregar_al_carrito'),
